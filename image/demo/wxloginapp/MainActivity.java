3799220b5818a2d1286072f96699513f9d834530
shelvey261602 <157129875866@163.com>
2018-07-06 10:45:15 +0800

    7.6-10:44

--- image/demo/wxloginapp/MainActivity.java
+++ image/demo/wxloginapp/MainActivity.java
@@ -12,6 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         string = new String();
-        string = "121212121";
     }
 }
